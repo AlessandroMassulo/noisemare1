255d37a51031057bdeb6cf8417214c9db44b4d13
Alessandro <alessandro.massulo@gmail.com>
2020-07-28 13:47:46 -0400

    Segunda alteracao

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+function CadastroVideo(){
+  return (
+    <div>
+      pagina de cadastro de video
+    </div>
+  )
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter> 
+    <Switch>
+     <Route path="/cadastro" component={CadastroVideo} />
+     <Route path="/" component={App} />
+     </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
