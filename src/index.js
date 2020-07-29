d499ad31affaf5394a7a5580ec092ba4bcf500d2
Alessandro <alessandro.massulo@gmail.com>
2020-07-28 23:06:53 -0400

    Third Edit

--- src/index.js
+++ src/index.js
@@ -2,27 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CadastroVideo from './pages/cadastro/Video/index';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-function CadastroVideo(){
-  return (
-    <div>
-      pagina de cadastro de video
-    </div>
-  )
-}
-
-
+// desafio - inserir um pagina com jogo no 404  (tipo Google)
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
   <BrowserRouter> 
     <Switch>
-     <Route path="/cadastro" component={CadastroVideo} />
-     <Route path="/" component={App} />
+     <Route path="/cadastro/video" component={CadastroVideo} />
+     <Route path="/cadastro/categoria" component={CadastroCategoria} />
+     <Route path="/" component={App} exact/>
+     <Route component={Pagina404}/>
      </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
-
 
